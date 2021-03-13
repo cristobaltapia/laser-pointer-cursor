a0e194c3139a60b8a2672ad87e300133946bfc1d
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-03-13 14:51:56 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ROOT_UID=0
+DEST_DIR=
+
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  DEST_DIR="/usr/share/icons"
+else
+  DEST_DIR="$HOME/.local/share/icons"
+fi
+
+if [ -d "$DEST_DIR/laser-pointer-cursors" ]; then
+  rm -r "$DEST_DIR/laser-pointer-cursors"
+fi
+
+mkdir -p $DEST_DIR && cp -pr dist $DEST_DIR/laser-pointer-cursors
+
+echo "Finished..."
+
